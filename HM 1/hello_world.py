03bffc1923d724610b333be582f6a7c68524354d
uberchurch <uberchurch@gmail.com>
2013-11-04 14:34:07 -0500

    Week 1 homework
    The first weeks homework

---
+++ HM 1/hello_world.py
@@ -0,0 +1,5 @@
+# Timothy S Brower
+# Hello_World
+# 10/26/13
+
+print 'Hello, World!'
---
+++ HM 1/hw1.py
@@ -0,0 +1,62 @@
+# Name: Timothy S Brower
+# Section: 
+# Date:10/26/13
+# hw1.py
+
+##### Template for Homework 1, exercises 1.2-1.5 ######
+
+print 'hello, world!'
+
+
+print "********** Exercise 1.2 **********"
+
+# Do your work for Exercise 1.2 here
+print '  |  |  '
+print '--------'
+print '  |  |  '
+print '--------'
+print '  |  |  '
+
+
+print "********** Exercise 1.3 **********"
+
+# Do your work for Excercise 1.3 here. Hint - how many different
+# variables will you need?
+
+vrt = '  |  |  '
+hrz = '--------'
+print vrt
+print hrz
+print vrt
+print hrz
+print vrt
+
+
+print "********** Exercise 1.4 **********"
+print "********* Part II *************"
+
+one = (3*5)/(2+3)
+two = ((7+9)**(.5))*2
+three = (4-7)** 3
+four = (-19+100)**(.25)
+five = 6 % 4
+
+print one,', ', two,', ', three,', ', four,', ', five
+
+print "********* Part III *************"
+
+a = (5-2)*3
+b = 5-(2*3)
+print a, ',', b
+
+
+print "********** Exercise 1.5 **********"
+
+first_name = raw_input("What is your first name?")
+last_name = raw_input("what is your last name?")
+month = raw_input("What month were you born in?")
+day = raw_input("What day were you born on?")
+year = raw_input("What year were you born in?")
+
+print first_name, last_name, 'was born on', month, day + ",", year + "."
+                      
